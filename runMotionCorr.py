d8d5eeb56765fe5746d2228bf9d6a5d9c9bbf449
Michael Cianfrocco <michael.a.cianfrocco@gmail.com>
2016-03-30 13:09:33 -0700

    Updated repo, added advanced options for runMotion

--- runMotionCorr.py
+++ runMotionCorr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import shutil
 import optparse
@@ -11,25 +11,29 @@ from os import system
 import linecache
 import time
 
-
+#This script runs IMOD and motioncorrection on a large number of movies in .mrc format
 #=========================
 def setupParserOptions():
         parser = optparse.OptionParser()
-        parser.set_usage("%prog --dir=<folder with micrographs> --bin=<binning>")
+        parser.set_usage("%prog --dir=<folder with mrc frames> --gain_ref=<gain reference in mrc format with full path;input the *_norm* file from the leginon reference directory> --save_bin <save binned mic> --save_norm <save normalized frames>\n\nThis program takes movies with .frames.mrcs extensions and creates aligned movies with .mrc extension, along with the option to create normalized movies with the .mrcs extension.")
         parser.add_option("--dir",dest="dir",type="string",metavar="FILE",
-                help="Directory containing direct detector movies with .mrcs extension")
-	parser.add_option("--bin",dest="bin",type="int",metavar="INTEGER",default=1,
-                help="Binning before movie alignment (Default = 1)")
-        parser.add_option("--save", action="store_true",dest="save",default=False,
-                help="Saved aligned, binned image for easy viewing")
+                    help="Directory containing direct detector movies (.frames.mrcs extension)")
+        parser.add_option("--gain_ref",dest="gain_ref",type="string",metavar="FILE",
+                    help="Gain reference file from Leginon with the full path (.mrc)")
+        parser.add_option("--save_bin", action="store_true",dest="save_bin",default=False,
+                    help="Save binned image for quick inspection")
+        parser.add_option("--save_norm", action="store_true",dest="save_norm",default=False,
+                    help="Save normalized movie frames as .mrcs")
+        parser.add_option("--bin",dest="bin",type="int",metavar="INT",default=1,
+                    help="Binning factor to use during movie alignment, 1 or 2. (Default=1)")
 	parser.add_option("-d", action="store_true",dest="debug",default=False,
-                help="debug")
+                    help="debug")
+
         options,args = parser.parse_args()
 
         if len(args) > 0:
                 parser.error("Unknown commandline options: " +str(args))
-
-        if len(sys.argv) < 2:
+        if len(sys.argv) <= 1:
                 parser.print_help()
                 sys.exit()
         params={}
@@ -42,76 +46,135 @@ def setupParserOptions():
 def checkConflicts(params):
 
         if not os.path.exists(params['dir']):
-                print "\nError: directory %s doesn't exists, exiting.\n" %(params['dir'])
+                print "\nError: input directory %s doesn't exists, exiting.\n" %(params['dir'])
+                sys.exit()
+        if not os.path.exists(params['gain_ref']):
+                print "\nError: input gain reference file %s dosen't exist, exiting.\n" %(params['gain_ref'])
                 sys.exit()
 
-	#Check if CUDA is loaded
-	cudapath = subprocess.Popen("env | grep cuda", shell=True, stdout=subprocess.PIPE).stdout.read().strip()	
-	if not cudapath:
-		print "\nError: CUDA libraries are not loaded. Load CUDA and try again.\n"
-		sys.exit()
 #==============================
-def alignDDmovies(params,motionCorrPath):
+def normalize(params,unblurPath):
 
-	mrcsList = sorted(glob.glob('%s/*.mrcs'%(params['dir'])))
-	currentPath = os.getcwd()
+    if not os.path.exists('%s_rot-180.mrc'%(params['gain_ref'][:-4])):
+    	cmd = 'newstack -rot -180 %s %s_rot-180.mrc' %(params['gain_ref'], params['gain_ref'][:-4])
+    	print cmd
+    	subprocess.Popen(cmd,shell=True).wait()
 
-	for mrcs in mrcsList:
-		
-		if params['debug'] is True:
-			print mrcs
+    if not os.path.exists('%s_rot-180_flipy.mrc'%(params['gain_ref'][:-4])):
+    	cmd = 'clip flipy %s_rot-180.mrc %s_rot-180_flipy.mrc' %(params['gain_ref'][:-4], params['gain_ref'][:-4])
+    	print cmd
+    	subprocess.Popen(cmd,shell=True).wait()
 
-		if os.path.exists('%s.mrc' %(mrcs[:-5])):
-			print "\n"
-			print 'Micrograph %s.mrc already exists, skipping %s' %(mrcs[:-5],mrcs)
-			print "\n"
-			continue
+    mrcList = sorted(glob.glob('%s/*.frames.mrcs'%(params['dir'])))
 
-		print 'Motion correcting movie %s --> %s.mrc' %(mrcs,mrcs[:-5])
+    if len(mrcList) == 0: 
+	print 'Error: No .frames.mrcs movies found in directory %s' %(params['dir'])
+	sys.exit()
 
-		cmd = '%s %s -fcs %s.mrc -bin %s' %(motionCorrPath,mrcs,mrcs[:-5],str(params['bin']))
-		if params['debug'] is True:
-			print cmd 
-		subprocess.Popen(cmd,shell=True).wait()
-	
-		mrcsOnly = mrcs.split('/')
+    for mrcs in mrcList:
+        if params['debug'] is True:
+            print 'Working on movie %s' %(mrcs)
+        
 
-		if params['save'] is True:
-			print 'Saving binned image'
-			if params['debug'] is True:
-				print 'Moving %s/dosef_quick/%s_CorrSum.mrc --> %s_CorrSum.mrc'%(params['dir'],mrcsOnly[-1][:-5],mrcs[:-5])
-			shutil.move('%s/dosef_quick/%s_CorrSum.mrc'%(params['dir'],mrcsOnly[-1][:-5]),'%s_CorrSum.mrc'%(mrcs[:-5]))
-	
-		#Clean up	
-		if params['bin'] == 1:
-			os.remove('%s_Log.txt' %(mrcs[:-5]))
-		if params['bin'] > 1:
-			os.remove('%s_%ix_Log.txt' %(mrcs[:-5],params['bin']))
-		shutil.rmtree('dosef_quick')	
-		
+        filename = '%s' %(mrcs)
+        split_file = filename.split('_')
+        split_line = filename.split('/')
+        actual_file_name = '%s' %(split_line[-1])
+        name_a = '%s' %(split_file[-1])
+
+        if name_a=='normalized.mrc':
+            continue 
+        if name_a=='SumCorr.mrc':
+            continue
+        if name_a=='CorrSum.mrc':
+            continue
+        if name_a=='1.mrc':
+            continue
+        if name_a=='0.mrc':
+            continue
+        if name_a=='flipy.mrc':
+            continue
+        if name_a=='rot-180.mrc':
+            continue
+        
+	inmovie=mrcs
+	normmovie='%s.mrcs' %(mrcs[:-12])
+	norm='%s.mrc' %(mrcs[:-12])
+
+	if params['debug'] is True:
+		print 'Input movie: %s' %(inmovie)
+		print 'Normalized output movie filename: %s' %(normmovie)
+		print 'Output aligned movie filename: %s' %(norm)
+
+        if os.path.exists(norm):
+            print "\n"
+            print 'Micrograph %s already exists, skipping %s' %(norm,inmovie)
+            print "\n"
+            continue
+        
+	cmd = '%s %s -fcs %s -hgr 0 -fgr %s_rot-180_flipy.mrc -bin %i' %(unblurPath,inmovie,norm,params['gain_ref'][:-4],params['bin'])
+	if params['debug'] is True:
+		print cmd
+        subprocess.Popen(cmd,shell=True).wait()
+
+        cmd = 'mv %s.mrc %s/' %(actual_file_name[:-12],params['dir'])
+        if params['debug'] is True:
+		print cmd
+        subprocess.Popen(cmd,shell=True).wait()
+
+        if params['save_bin'] is True:
+            cmd = 'mv dosef_quick/%s_normalized_CorrSum.mrc %s/%s_dosefQuick.mrc' %(actual_file_name[:-12],params['dir'],actual_file_name[:-12])
+            if params['debug'] is True:
+		print cmd
+            subprocess.Popen(cmd,shell=True).wait()
+
+        if params['save_norm'] is True:  
+            cmd = 'clip mult -n 16 %s %s_rot-180_flipy.mrc %s' %(mrcs,params['gain_ref'][:-4],normmovie)
+	    if params['debug'] is True:
+                print cmd
+            subprocess.Popen(cmd,shell=True).wait()
+
+    cmd = 'rm -rf dosef_quick %s_Log.txt' %(actual_file_name[:-12])
+    if params['debug'] is True:
+	print cmd
+    subprocess.Popen(cmd,shell=True).wait()       
 #=============================
-def getMotionCorrPath(params):
+def getimodPath():
 
-	#first try same directory as this script
-	currentPath = sys.argv[0]
-	currentPath = currentPath[:-30]
+        imodpath = subprocess.Popen("env | grep IMOD_DIR", shell=True, stdout=subprocess.PIPE).stdout.read().strip()
+        if imodpath:
+                return imodpath
+        print "IMOD was not found, make sure IMOD is in your path"
+        sys.exit()
+#=============================
+def getunblurPath(params):
 
-	if os.path.exists('%s/motioncorr' %(currentPath)):
-		return '%s/motioncorr' %(currentPath)
+        #first try same directory as this script
+        currentPath = sys.argv[0]
+        currentPath = currentPath[:-23]
 
-	if os.path.exists('motioncorr'):
-		return 'motioncorr'
+        if os.path.exists('%s/motioncorr_v2.1/bin/dosefgpu_driftcorr' %(currentPath)):
+                return '%s/motioncorr_v2.1/bin/dosefgpu_driftcorr' %(currentPath)
 
-	if os.path.exists('/usr/local/bin/motioncorr'):
-		return '/usr/local/bin/motioncorr'
-	
-	if os.path.exists('/home/EM_Packages/motioncorr_v2.1/bin/dosefgpu_driftcorr'):
-                return '/home/EM_Packages/motioncorr_v2.1/bin/dosefgpu_driftcorr'
-		
+        if os.path.exists('motioncorr_v2.1/bin/dosefgpu_driftcorr'):
+                return 'motioncorr_v2.1/bin/dosefgpu_driftcorr'
+
+        if os.path.exists('/home/indrajit/Desktop/Scripts/motioncorr_v2.1/bin/dosefgpu_driftcorr'):
+                return '/home/indrajit/Desktop/Scripts/motioncorr_v2.1/bin/dosefgpu_driftcorr'
+
+
+        print "\nError: path to motioncorr not found, exiting.\n"
+        sys.exit()
 #==============================
 if __name__ == "__main__":
 
         params=setupParserOptions()
         checkConflicts(params)
-	motionCorrPath=getMotionCorrPath(params)
-	alignDDmovies(params,motionCorrPath)
+        imodpath=getimodPath()
+        unblurPath=getunblurPath(params)
+        normalize(params,unblurPath)
+
+	#CLeanup 
+	if os.path.exists('%s_rot-180.mrc' %(params['gain_ref'])):
+		os.remove('%s_rot-180.mrc' %(params['gain_ref']))
+		os.remove('%s_rot-180_flipy.mrc' %(params['gain_ref']))
